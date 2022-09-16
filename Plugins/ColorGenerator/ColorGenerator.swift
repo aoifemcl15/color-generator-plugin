7eb02377a024162e8d153fe63ee9aa4605438eb2
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2022-09-16 16:40:27 +0100

    try another approach of finding the correct files

--- Plugins/ColorGenerator/ColorGenerator.swift
+++ Plugins/ColorGenerator/ColorGenerator.swift
@@ -34,13 +34,14 @@ extension ColorGenerator: XcodeBuildToolPlugin {
 
     // Required function to enable functionality to work with Xcode project
     func createBuildCommands(context: XcodeProjectPlugin.XcodePluginContext, target: XcodeProjectPlugin.XcodeTarget) throws -> [PackagePlugin.Command] {
-        print("TESTING inputFiles \(target.inputFiles)")
-        let jsonFiles = target.inputFiles.filter { $0.path.lastComponent == "json" }
-        print("TESTING jsonFiles count: \(jsonFiles.count)")
-        guard let semanticJson = jsonFiles.first(where: { $0.path.string == "Semantic"})?.path,
-              let paletteJson = jsonFiles.first(where: { $0.path.string == "Palette"})?.path else {
+
+        let resourceFiles = target.inputFiles.filter { $0.type == .resource }
+        guard let semanticJson = resourceFiles.first { $0.path.lastComponent == "Semantic.json" }?.path,
+        let paletteJson = resourceFiles.first { $0.path.lastComponent == "Palette.json" }?.path else {
             return []
         }
+
+        print("INPUTFILES: \(semanticJson), \(paletteJson)")
 
         // TODO: figure out how to add files to the target
 //        let outPut = target.appending(["TestGeneratedColorOutput"])
