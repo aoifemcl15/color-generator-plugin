c31544757ffc98dc28929aaa6ee7d91413c669cf
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2022-09-30 17:10:24 +0100

    rename to avoid clash with existing Colors.swift in main project

--- Plugins/ColorGeneratorPlugin/ColorGeneratorPlugin.swift
+++ Plugins/ColorGeneratorPlugin/ColorGeneratorPlugin.swift
@@ -53,7 +53,7 @@ extension ColorGenerator: XcodeBuildToolPlugin {
         Diagnostics.remark("Palette json path: \(paletteJsonPath)")
         Diagnostics.remark("Package output path: \(outputPath)")
 
-        let colorsOutput = outputPath.appending(subpath: "Colors.swift")
+        let colorsOutput = outputPath.appending(subpath: "GeneratedColors.swift")
 
         return [.buildCommand(displayName: "Generating color assets",
                               executable: try context.tool(named: "ColorGeneratorExec").path,
